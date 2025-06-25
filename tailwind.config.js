e61b0d4f94259638c6a310cd3d9db3a9abe82a2a
GhostHost256 <79661905+GhostHost256@users.noreply.github.com>
2025-06-25 02:37:09 -0400

    Safelist flicker class for production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  safelist: ['flicker'], // 👈 Add this line to prevent purge
   darkMode: 'class',
   theme: {
     extend: {},
